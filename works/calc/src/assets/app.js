41f26f9c0e4fcbe21eae05b1b8c46022e6bc8d91
mamazhanov <book.sites9@gmail.com>
2021-06-09 17:03:38 +0600

    calc done

--- works/calc/src/assets/app.js
+++ works/calc/src/assets/app.js
@@ -12,22 +12,44 @@ let slider_sum = document.getElementById("sum");
 let earnings_output = document.getElementById("earnings_output");
 //Ежемесячная выплата
 let pay_output = document.getElementById("pay_output");
+//Проценты
+let year_percent_output = document.getElementById("year_percent_output");
 
 const calc = () => {
     let profit = sum * yearPercent * month * 30 / (30 * 12) / 100;
-    let everyMonthProfit = profit / month;
+    let allSum = profit + sum;
+    let everyMonthProfit = allSum / month;
+
+    console.log(allSum)
+
     earnings_output.innerHTML = new Intl.NumberFormat().format(profit);
-    pay_output.innerHTML = new Intl.NumberFormat().format(everyMonthProfit);
+    pay_output.innerHTML = Math.floor(everyMonthProfit);
+    year_percent_output.innerHTML = yearPercent + "%"
+
 }
 
 const sum_slider = () => {
-    sum = slider_sum.value;
+    sum = +slider_sum.value;
     document.getElementById("investments_span").innerHTML = new Intl.NumberFormat().format(slider_sum.value);
     calc();
 }
 
 const month_slider = () => {
-    month = slider_month.value;
+    if (slider_month.value <= 17) {
+        yearPercent = 15;
+        document.getElementById("year_percent_output").innerHTML = "15%";
+    }
+    else if (slider_month.value <= 23) {
+        yearPercent = 20;
+        document.getElementById("year_percent_output").innerHTML = "20%";
+    }
+    else if (slider_month.value >= 24) {
+        yearPercent = 24;
+        document.getElementById("year_percent_output").innerHTML = "24%";
+    }
+    month = +slider_month.value;
     document.getElementById("month_span").innerHTML = slider_month.value;
     calc();
 }
+
+calc();
